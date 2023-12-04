bec2d8eb2f46568babed8c39c283210d49cab2ea
ramgoenka08 <98142966+RamGoenka08@users.noreply.github.com>
2023-12-03 23:23:42 -0600

    Add files via upload

---
+++ code/frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import LoginPage from './LoginPage';
+
+const App = () => {
+    return (
+        <div>
+            <LoginPage />
+        </div>
+    );
+};
+
+export default App;
+
